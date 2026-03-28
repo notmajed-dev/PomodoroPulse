775c3d6d126437bf119e4621bd950aea0f52291a
Majed <notmajed.js@gmail.com>
2026-03-28 09:59:55 +0530

    Add Stats component with Chart.js integration

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const Stats = ({ stats, taskList }) => {
   const data = {
